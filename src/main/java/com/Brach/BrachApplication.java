7ee0af8be70840d93c8a522a5af0c67eb70b0993
farook <mdfarook3591@gmail.com>
2026-09-16 20:57:48 +0530

    commited2

--- src/main/java/com/Brach/BrachApplication.java
+++ src/main/java/com/Brach/BrachApplication.java
@@ -8,6 +8,7 @@ public class BrachApplication {
 int x = 10;
 int y = 20;
 int y=30
+String s = "farook";
 System.out.println(100);
 System.out.println("bug-fix");
 	public static void main(String[] args) {
