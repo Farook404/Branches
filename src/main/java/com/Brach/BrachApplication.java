7d1e9c4f8941dd92cb991bbcef7e42eac8c3a989
Farook404 <farook7483@gmail.com>
2026-09-16 19:20:56 +0530

    Remove unnecessary blank line in BrachApplication

--- src/main/java/com/Brach/BrachApplication.java
+++ src/main/java/com/Brach/BrachApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BrachApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BrachApplication.class, args);
 	}
