5e1136a7ee6615012b2fd403f7809dc2c4359ff9
Farook404 <farook7483@gmail.com>
2026-09-16 19:08:42 +0530

    Add variable 'y' and debug print statement
    Added a new variable 'y' and a print statement for debugging.

--- src/main/java/com/Brach/BrachApplication.java
+++ src/main/java/com/Brach/BrachApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrachApplication {
 int x = 10;
+	int y = 20;
 System.out.println("bug-fix");
 	public static void main(String[] args) {
 		SpringApplication.run(BrachApplication.class, args);
