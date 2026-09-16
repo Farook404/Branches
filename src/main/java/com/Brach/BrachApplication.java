f2c380b0765ad3348cdf1999528a5d5de704a970
farook <mdfarook3591@gmail.com>
2026-09-16 16:28:58 +0530

    Defectfix-3456-login-error

--- src/main/java/com/Brach/BrachApplication.java
+++ src/main/java/com/Brach/BrachApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BrachApplication {
-
+int x = 10;
+System.out.println("bug-fix");
 	public static void main(String[] args) {
 		SpringApplication.run(BrachApplication.class, args);
 	}
